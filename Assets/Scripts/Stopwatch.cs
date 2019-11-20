ad1b840cdd82eb6209cf471c16314ed9f28a3eae
Muhammad Umar Fadhlullah <ufadhlullah@gmail.com>
2019-11-21 04:40:49 +0700

    Add GameScene layout and add assets

---
+++ Assets/Scripts/Stopwatch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stopwatch : MonoBehaviour
+{
+    Text text;
+    float currentTime;
+    [SerializeField] float speed = 1;
+    bool playing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        playing = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playing && currentTime < 3600)
+        {
+            currentTime += Time.deltaTime * speed;
+            string minutes = Mathf.Floor((currentTime % 3600) / 60).ToString("00");
+            string seconds = (currentTime % 60).ToString("00");
+            text.text = minutes + ":" + seconds;
+        }
+        else
+        {
+            text.text = "Time's up!";
+        }
+    }
+}
